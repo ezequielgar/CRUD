579e42e287bd580250d3ddd1269d1232af639d6c
Ezequiel Garcia <ezequielgar@gmail.com>
2021-11-29 17:57:48 -0300

    se completa la primera parte del form

--- js/admin.js
+++ js/admin.js
@@ -2,9 +2,11 @@ function campoRequerido(input) {
   if (input.value.trim().length > 0) {
     //console.log("paso la validacion");
     input.className = "form-control is-valid";
+    return true;
   } else {
     //console.log("no pasa la validacion");
     input.className = "form-control is-invalid";
+    return false;
   }
 }
 
@@ -12,34 +14,48 @@ function validarNros(input) {
   // crear una expresion regular
   let patron = /^[0-9]{1,3}$/;
   // probar el funcionamiento del patro o expresion regular
-  if(patron.test(input.value)){
+  if (patron.test(input.value)) {
     // cumple la expresion regular
     input.className = "form-control is-valid";
-  }else{
+    return true;
+  } else {
     // si no cumple la expresion regular
     input.className = "form-control is-invalid";
+    return false;
   }
-
 }
 
-function validarURL(input){
+function validarURL(input) {
   let patron = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
 
-  if (patron.test(input.value)){
+  if (patron.test(input.value)) {
     input.className = "form-control is-valid";
-
-  } else{
+    return true;
+  } else {
     input.className = "form-control is-invalid";
+    return false;
   }
-    
-  }
-
-function validarGral(e){
-  e.preventDefault();
-  console.log("funcion validar gral")
-
 }
 
+function validarGral(e) {
+  e.preventDefault();
+  console.log("funcion validar gral");
+  //volver a validar todos los campos
+  if (
+    campoRequerido(campoCodigo) &&
+    campoRequerido(campoProducto) &&
+    campoRequerido(campoDescripcion) &&
+    validarNros(campoCantidad) &&
+    validarURL(campoURL)) {
+    console.log("si paso la validacion");
+    let alerta = document.getElementById("msjAlerta");
+    alerta.className = "alert alert-danger my-2 d-none";
+  } else {
+    console.log("no paso la validacion");
+    let alerta = document.getElementById("msjAlerta");
+    alerta.className = "alert alert-danger my-2";
+  }
+}
 
 //agregar eventos a los elementos del formulario
 
@@ -65,8 +81,12 @@ campoDescripcion.addEventListener("blur", () => {
   campoRequerido(campoDescripcion);
 });
 
-campoCantidad.addEventListener("blur", () => {validarNros(campoCantidad);});
+campoCantidad.addEventListener("blur", () => {
+  validarNros(campoCantidad);
+});
 
-campoURL.addEventListener('blur', () => {validarURL(campoURL);});
+campoURL.addEventListener("blur", () => {
+  validarURL(campoURL);
+});
 
-formularioProducto.addEventListener('submit',validarGral);
+formularioProducto.addEventListener("submit", validarGral);
